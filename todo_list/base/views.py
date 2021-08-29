397a033baf50f07ec8bcaebe3facb6de6283709a
Rohit Abhishek <rabhi1611@gmail.com>
2021-08-29 19:57:40 +0530

    added CRUD feature

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,7 +1,34 @@
+from django.db import models
+from base.models import Task
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
+from .models import Task
 
 # Create your views here.
-def taskList(request):
-    return HttpResponse('To Do list')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
